da8453aca982b6ffcae52561373d2eacdeeb74d1
Seungwan Jung <antitheseaboutcommonsense@gmail.com>
2022-01-12 05:39:19 +0900

    separate interest

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -8,7 +8,8 @@ import hello.core.member.MemberServiceImple;
 public class MemberApp {
 
     public static void main(String[] args) { //psvm
-        MemberService memberService = new MemberServiceImple();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP); //long이라 1L, 1넣으면 컴파일오류
         memberService.join(member);
 
