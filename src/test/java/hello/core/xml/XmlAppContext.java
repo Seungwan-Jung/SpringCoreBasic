cbab11ba19f33ae30a2dc86b7d9acba834289761
seungwan-jung <saddler-tenure0i@icloud.com>
2022-01-22 02:48:46 +0900

    Bean setting Meta info

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -13,7 +13,7 @@ public class XmlAppContext {
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
         MemberService memberService = ac.getBean("memberService", MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
-        
+
 
     }
 }
